d1c9efb61d64cef81fdcb06ed52ee414c4af9e7d
MirkaaStefanov <mirkanstefanov2007@gmail.com>
2024-04-19 22:23:10 +0300

    Deleted inability

--- src/main/java/com/example/NAMEevents/Inability/InabilityRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.NAMEevents.Inability;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface InabilityRepository extends CrudRepository<Integer,Inability> {
-}
