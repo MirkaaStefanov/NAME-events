c3a0afec58a18c160d8dcea1326d14a814908bff
MirkaaStefanov <mirkanstefanov2007@gmail.com>
2024-04-20 16:18:31 +0300

    Messages

--- src/main/java/com/example/NAMEevents/Message/MessageRepository.java
+++ src/main/java/com/example/NAMEevents/Message/MessageRepository.java
@@ -3,10 +3,13 @@ package com.example.NAMEevents.Message;
 import com.example.NAMEevents.User.User;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface MessageRepository extends CrudRepository<Message, Integer> {
 
-    List<Message> findBySenderAndReceiver(User sender, User receiver);
+    @Query("SELECT m FROM Message m WHERE m.sender = :sender AND m.receiver = :receiver OR m.sender = :receiver AND m.receiver = :sender")
+    List<Message> findBySenderAndReceiver(@Param("sender") User sender, @Param("receiver") User receiver);
+
 }
